70466696a5fef9a9e3daca0d46c334c3f8bf229c
Rodrigo <rodrigo.ldovr@gmail.com>
2023-03-02 11:47:08 -0300

    Criado componente campo

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,9 +1,13 @@
-import Banner from "./componentes/Banner/Banner";
+import Banner from "./componentes/Banner";
+import CampoTexto from "./componentes/CampoTexto";
 
 function App() {
    return (
       <div className="App">
          <Banner />
+         <CampoTexto label="Campeão"  placeholder="Digite o nome do campeão" />
+         <CampoTexto label="Alcance de ataque" placeholder="Digite se o campeão tem Ranged ou Melee" />
+         <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem do campeão"/>
       </div>
    );
 }
